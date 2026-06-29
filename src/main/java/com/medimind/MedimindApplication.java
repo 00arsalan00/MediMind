9ddd7e52e70dff548a75a49cb1bd277d4c2cdd96
Arsalan <2k23.psitaiml2311749@gmail.com>
2026-06-29 22:35:15 +0530

    Initialization

---
+++ src/main/java/com/medimind/MedimindApplication.java
@@ -0,0 +1,13 @@
+package com.medimind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedimindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medimind/MedimindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medimind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
